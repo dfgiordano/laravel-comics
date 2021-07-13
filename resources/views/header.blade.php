dd447fd196a78c731cf67ffbbad84e8c170329c6
dfgiordano <federico.giordano90@gmail.com>
2021-07-13 16:25:58 +0200

    impostazione header

---
+++ resources/views/header.blade.php
@@ -0,0 +1,49 @@
+<header>
+    <section class="section_h_1">
+
+    </section>
+    <section class="section_h_2 container">
+        <img src="{{ asset('images/dc-logo.png') }}" alt="dc-logo">
+        <ul>
+            <li>
+                <a href="#">characters</a>
+            </li>
+            <li class="active">
+                <a href="#">comics</a>
+            </li>
+            <li>
+                <a href="#">movies</a>
+            </li>
+            <li>
+                <a href="#">tv</a>
+            </li>
+            <li>
+                <a href="#">games</a>
+            </li>
+            <li>
+                <a href="#">collectibles</a>
+            </li>
+            <li>
+                <a href="#">videos</a>
+            </li>
+            <li>
+                <a href="#">fans</a>
+            </li>
+            <li>
+                <a href="#">news</a>
+            </li>
+            <li>
+                <a href="#">shop</a>
+            </li>
+        </ul>
+        <div class="search-container">
+            <form action="/action_page.php">
+              <input type="text" placeholder="Search.." name="search">
+              <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+          </div>
+    </section>
+    <section class="jumbotron">
+        
+    </section>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $comicsArray = config("mioDb");
+
+    // dump($comicsArray);
+    return view('home', [
+        "comicsArray" => $comicsArray
+    ]
+);
 });
